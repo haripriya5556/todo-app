db3fd2b97b95bac5eb5d591ae22039d15445652b
haripriya5556 <deepu.sai88@gamil.com>
2016-09-09 13:10:05 +0530

    added static components

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,9 +1,27 @@
 var React=require('react');
+var TodoList=require('TodoList');
 var TodoApp=React.createClass({
+  getInitialState:function(){
+    return {
+      todos:[
+        {
+        id:1,
+        text:'hi'
+      }, {
+        id:2,
+        text:'hello'
+      }, {
+        id:3,
+        text:'hey'
+      }
+      ]
+    };
+  },
   render:function (){
+    var {todos}=this.state;
     return (
       <div>
-        todooo
+  <TodoList todos={todos}/>
       </div>
     )
   }
